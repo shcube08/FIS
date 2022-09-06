bad6eea53015bb3773b7ade6a85ba5c7cd04a166
Shubham Shinde <shubhamiiitdwd@gmail.com>
2022-09-06 21:28:54 +0530

    Update Assignment4_senario_based.sql

--- Assignment4_senario_based.sql
+++ Assignment4_senario_based.sql
@@ -4,7 +4,7 @@ insert into tblBook values
 ('My Second SQL book', 'John Mayer',5428570112,'1972-02-22 12:08:17.260'),
 ('My Third SQL book', 'Cary Flint',1641777152,'2015-02-22 12:08:17.260');
 
-________________________________________________________________________________________________________________________________
+--________________________________________________________________________________________________________________________________
 select * from tblBook where author like '%er';
 
 drop table tblReview;
@@ -17,54 +17,54 @@ insert into tblReview values
 (2,'John Smith', 'Second review',4,'1972-02-22 12:08:17.260'),
 (3,'Alice waker', 'Third review',5,'2015-02-22 12:08:17.260');
 
-________________________________________________________________________________________________________________________________
+--________________________________________________________________________________________________________________________________
 select title, author, reviewer_name as 'reviewer name' from tblBook, tblReview where tblBook.id = tblReview.book_id;
 
-________________________________________________________________________________________________________________________________
+--________________________________________________________________________________________________________________________________
 select reviewer_name from tblReview group by reviewer_name having count(reviewer_name) > 1;
 
 
 create table tblCustomer(id int identity(1,1) primary key, name varchar(20),  age numeric(4), address varchar(max), salary numeric(10));
 create table tblOrder(int int identity(1, 1)primary key, odr_date date, customer_id int foreign key references tblCustomer(id), amount numeric(10))
 
-________________________________________________________________________________________________________________________________
+--________________________________________________________________________________________________________________________________
 select * from tblCustomer where address in (select address from tblCustomer where address like  '%o%' group by address having count(address) > 1);
 
 
-________________________________________________________________________________________________________________________________
+--________________________________________________________________________________________________________________________________
 select count(*) ' Total number of customers placed on same date ', odr_date 'order date' from tblCustomer c1 join tblOrder o1 on c1.id = o1.customer_id group by odr_date having count(*) > 1;
 
-________________________________________________________________________________________________________________________________
+--________________________________________________________________________________________________________________________________
 select lower(ename) 'Employee name' from tblEMP where salary is null; 
 
 
 
 create table tblStudentDetails(registerNo int primary key identity(1,1), Name varchar(20), age numeric(4), qualification varchar(10), mobileNo numeric(10), mailId varchar(40), Location varchar(20), gender char); 
-________________________________________________________________________________________________________________________________
+--________________________________________________________________________________________________________________________________
 select gender, count(*) 'Gender count'from tblStudentDetails group by gender;
 
 create table tblCourse(cid varchar(6) primary key , c_name varchar(20), start_date date, end_date date, fee numeric(4));
 create table tblCourseRegistration(registerNo int primary key identity(1,1), cid  varchar(6) foreign key references tblCourse(cid), batch varchar(4));
 
-________________________________________________________________________________________________________________________________
+--________________________________________________________________________________________________________________________________
 select c_name 'Course name', (select count(*) from tblCourseRegistration where cid = c.cid) 'Number of student registered' from tblCourse c where start_date > '2018-01-02' and end_date <= '2018-02-28' 
 
 
 create table tblCustomer2(c_id int identity(1,1) primary key, first_name varchar(10), last_name varchar(10));
 create table tblOrder2(order_id int identity(1,1) primary key, order_date date, amount numeric(10),	c_id int foreign key references tblCustomer2(c_id));	
 
-________________________________________________________________________________________________________________________________
+--________________________________________________________________________________________________________________________________
 select * from tblCustomer2 c where (select count(*) from tblOrder2 where c_id = c.c_id)  = 2;
 
-________________________________________________________________________________________________________________________________
+--________________________________________________________________________________________________________________________________
 select Name, upper(Location) as 'Location' from tblStudentDetails order by name desc; 
 
 create table tblOrder3(order_id int identity(1,1) primary key, order_date date, order_number numeric (20), amount numeric(10),	c_id int foreign key references tblCustomer2(c_id));	
 create table tblProduct(id int identity(1,1) primary key, product_name varchar(20), supplierId numeric(20), unitPrice numeric(4), package varchar(10), isDiscontinued numeric(1));
 create table tblOrderItem(id int identity(1,1) primary key, orderId int foreign key references tblOrder3(order_id), product_id int foreign key references tblProduct(id),unitPrice numeric(4), quantity numeric(20));
 
-________________________________________________________________________________________________________________________________
+--________________________________________________________________________________________________________________________________
 create view orderView as select product_name, quantity, order_number from tblOrder3 o join tblOrderItem oi on o.order_id = oi.orderId join tblProduct p on oi.product_id = p.id;
 
-________________________________________________________________________________________________________________________________
+--________________________________________________________________________________________________________________________________
 select c.c_name 'Course Name' from tblStudentDetails sd join tblCourseRegistration cr on sd.registerNo = cr.registerNo join tblCourse c on cr.cid = c.cid where sd.name = 'Neha';
